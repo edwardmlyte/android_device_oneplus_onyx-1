36966fd3a7caced28fea663d77847f55dad7a71c
Lennart <lennart.1997@gmx.de>
2016-09-12 17:18:30 +0200

    Oneplus3: fix sim card detection
    Change-Id: I57a127f8a786cd1b59191901868d2a674fc946f0
    
    Conflicts:
    	device.mk

--- device.mk
+++ device.mk
@@ -96,6 +96,34 @@ PRODUCT_PACKAGES += \
     libqcomvoiceprocessing \
     tinymix
 
+<<<<<<< HEAD
+=======
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:system/etc/mixer_paths_tasha.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
+
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
+PRODUCT_COPY_FILES += \
+    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
+
+# RIL
+PRODUCT_PACKAGES += \  
+    libcnefeatureconfig \
+    libxml2 \
+    libprotobuf-cpp-full 
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8974 \
